816e59ae6a786e04d8664fcace1c5cb5dc956ba5
Vaclav Pribik <vaclav.pribik@gmail.com>
2017-09-26 14:35:53 +0200

    ADD (0.9.6478.24276): High-DPI mode support

--- Silicups.GUI/MainForm.Designer.cs
+++ Silicups.GUI/MainForm.Designer.cs
@@ -641,8 +641,8 @@
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1166, 526);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.splitContainerMain);
